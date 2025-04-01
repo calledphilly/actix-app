f90c24c9573a317afe318b1f6148173c4bd04d8e
calledphilly <ybikouta@icloud.com>
2025-04-01 11:54:22 +0200

    Improve error handling in main function of Actix web application. Update environment variable unwrapping to use expect for clearer error messages. Increase maximum database connections from 2 to 5 and generate a cookie secret key.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_cors;
 use actix_session;
+use actix_web::cookie::Key;
 use dotenv;
 mod utils;
 use sqlx::postgres::PgPoolOptions;
@@ -8,21 +9,21 @@ use utils::services::{hello_handler, login_handler, logout_handler, users_handle
 #[actix_web::main] 
 async fn main() -> std::io::Result<()> {    
     dotenv::dotenv().ok();
-    let host = std::env::var("HOST").unwrap();
+    let host = std::env::var("HOST").expect("Error occured while unwrapping of HOST");
     let host_cloned = host.clone();
-    let port_actix_app = std::env::var("PORT_ACTIX_APP").expect("Error during unwrap of PORT_ACTIX_APP");
-    let port_yew_app = std::env::var("PORT_YEW_APP").expect("Error during unwrap of PORT_YEW_APP");
-    let postgres_url = std::env::var("POSTGRES_URL").expect("Error during unwrap of POSTGRES_URL");
-    let redis_url = std::env::var("REDIS_URL").expect("Error during unwrap of REDIS_URL");
-    let cookie_secret_key = actix_web::cookie::Key::generate();
+    let port_actix_app = std::env::var("PORT_ACTIX_APP").expect("Error occured while unwrapping of PORT_ACTIX_APP");
+    let port_yew_app = std::env::var("PORT_YEW_APP").expect("Error occured while unwrapping of PORT_YEW_APP");
+    let postgres_url = std::env::var("POSTGRES_URL").expect("Error occured while unwrapping of POSTGRES_URL");
+    let redis_url = std::env::var("REDIS_URL").expect("Error occured while unwrapping of REDIS_URL");
     let redis_store = actix_session::storage::RedisSessionStore::new(redis_url)
         .await
-        .unwrap();
+        .expect("Error occured while unwrapping of redis_store");
     let db_pool = PgPoolOptions::new()
-        .max_connections(2)
+        .max_connections(5)
         .connect(postgres_url.as_str())
         .await
-        .expect("Error during connection to database");
+        .expect("Error occured while connecting to the postgres's database");
+    let cookie_secret_key = Key::generate();
     
     println!("\nServer running on : http://{}:{}",host,port_actix_app);
     actix_web::HttpServer::new(move || {
